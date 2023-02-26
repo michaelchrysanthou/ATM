ca73298f1881f670a71f5e9a6b95b5155b67d58b
Michael Chrysanthou <115214818+michaelchrysanthou@users.noreply.github.com>
2023-02-26 11:20:05 -0800

    2.26.23_1.2

--- account1.jsx
+++ account1.jsx
@@ -1,35 +1,40 @@
-// this keeps a running total of deposits and withdrawals
-
-const ATMDeposit = ({ onChange }) => {
+const ATMDeposit = ({ onChange, isDeposit }) => {
+  const choice = ["Deposit", "Cash Back"];
+  console.log(`ATM isDeposit: ${isDeposit}`);
   return (
     <label className="label huge">
-      Deposit:
-      <input type="number" onChange={onChange}></input>
-      <input type="submit"></input>
+      <h3> {choice[Number(!isDeposit)]}</h3>
+      <input type="number" width="200" onChange={onChange}></input>
+      <input type="submit" width="200" value="Submit"></input>
     </label>
   );
 };
 
 const Account = () => {
-  const [accountState, setAccountState] = React.useState(0);
-  var deposit = 0;
+  let deposit = 0; // state of this transaction
+  const [totalState, setTotalState] = React.useState(0);
+  const [isDeposit, setIsDeposit] = React.useState(true);
+
+  let status = `Account Balance $ ${totalState} `;
+  console.log(`Account Rendered with isDeposit: ${isDeposit}`);
   const handleChange = event => {
     console.log(`handleChange ${event.target.value}`);
     deposit = Number(event.target.value);
   };
-  const handleSubmit = event => {
-    let newTotal = accountState + deposit;
-    alert(`Account total = ${newTotal}`);
-    setAccountState(newTotal);
+  const handleSubmit = () => {
+    let newTotal = isDeposit ? totalState + deposit : totalState - deposit;
+    setTotalState(newTotal);
     event.preventDefault();
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <h2>Account Balance {accountState}</h2>
-      <ATMDeposit onChange={handleChange}> Deposit</ATMDeposit>
+      <h2 id="total">{status}</h2>
+      <button onClick={() => setIsDeposit(true)}>Deposit</button>
+      <button onClick={() => setIsDeposit(false)}>Cash Back</button>
+      <ATMDeposit onChange={handleChange} isDeposit={isDeposit}></ATMDeposit>
     </form>
   );
 };
 // ========================================
-ReactDOM.render(<Account />, document.getElementById("root"));
+ReactDOM.render(<Account />, document.getElementById("root"));;
